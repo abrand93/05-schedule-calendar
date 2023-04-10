d9a4f46a0619b8007a1da350da95f62ea8a6b90b
Verbal Kent <abrandstrom93@gmail.com>
2023-04-10 11:19:50 -0500

    add coments to the javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 var today = dayjs()
+// showing the current date time. 
 $('#currentDay').text(today.format('MMM D,YYYY, h:mm:ss'))
 //format the current time with the 24 hour clock.
-var currentHour = today.format('H')
+var currentHour = parseInt(today.format('H'))
 
 var saveButton = document.querySelectorAll(".btn")
 // using Jquery to grab the elemnts ID.
@@ -27,33 +28,42 @@ $("#hour-16 .description").val(localStorage.getItem('hour-16'))
 $("#hour-17 .description").val(localStorage.getItem('hour-17'))
 
 $(function () {
+  //event listnerer on click run this function
   $(".saveBtn").on("click", function () {
-
+// on click find this sibling with the class of description and the vaule
     var userInput = $(this).siblings('.description').val()
+    //When click find this parents attibute. In this case its ID. 
     var time = $(this).parent().attr('id')
+    // now we are setting theses Items into local storage.
     localStorage.setItem(time, userInput)
-
+    console.log($(this).parent().attr('id'))
+    console.log($(this).siblings('.description').val())
   }
   )
 
   $('.time-block').each(function () {
- // This changes removes hour- and will only be seen as the numbers.
+ // This block of code removes hour- and will only be seen as the numbers.
     var divtime = parseInt($(this).attr('id').split('hour-')[1])
     console.log(divtime)
     // with hour- removed we can now compare the current hour to the id number. 
     // We are using 24 hour clock to compair the numbers. 
+    
+   // if the container time is less the current time add the past class 
     if (divtime < currentHour) {
       $(this).addClass('past')
       $(this).removeClass('future')
       $(this).removeClass('present')
 
     }
-    else if (divtime == currentHour) {
+    // if the container time is is equal to current time add the present class to the container.
+    // 
+    else if (divtime === currentHour) {
       $(this).addClass('present')
       $(this).removeClass('past')
       $(this).removeClass('future')
 
     }
+    // if the container time is greater then the current time add futer class.  
     else if (divtime > currentHour) {
       $(this).addClass('future')
       $(this).removeClass('past')
